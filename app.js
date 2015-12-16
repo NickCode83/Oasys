fb2d20c30efb40150222c8de045f3884a89594f2
NickCode83 <yixinzhu@126.com>
2015-12-17 01:48:17 +0800

    添加用户登陆数据
    添加用户登陆数据模型，用户列表

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var User = require('./models/user.js');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -8,6 +10,10 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'jade');
 app.set('port', port);
 app.set('views', './views/pages');
+app.locals.moment = require('moment');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
 	res.render('index',{
@@ -55,6 +61,38 @@ app.get('/cloud', function(req, res){
 	res.render('cloud',{
 		title:'Oasys系统-网络硬盘'
 	});
+});
+
+//list page
+app.get('/admin/userlist',function(req,res){
+	
+	User.fetch(function(err, users){
+		if(err){
+			console.log(err);
+		}
+
+		res.render('userlist',{
+			title:'imooc 用户列表页',
+			users:users
+
+		});
+	});
+});
+
+//signup
+app.post('/user/signup',function(req,res){
+	var _user = req.body.user;
+
+	var user = new User(_user);
+
+	user.save(function(err,user){
+		if(err){
+			console.log(err);
+		}
+
+		console.log(user);
+	});
+	res.redirect('/');
 });
 
 app.use(express.static(path.join(__dirname,'public')));
--- schemas/user.js
+++ schemas/user.js
@@ -35,7 +35,7 @@ UserSchema.pre('save', function(next){
 		bcrypt.hash(user.password, salt, function(err, hash){
 			if(err) return next(err);
 
-			user.password = hash
+			user.password = hash;
 			next();
 		});
 	});
